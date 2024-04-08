1309fea9f6b11b37ee3485cc91268353e40eaeb5
Nicolas Fernandez <nicofesan@hotmail.com>
2024-04-08 16:36:45 -0500

    State storage

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -1,14 +1,22 @@
 import { useState } from "react"
 import confetti from "canvas-confetti"
 import { Square } from "./components/Square"
-import { TURNS, WINNER_COMBOS } from "./constants"
+import { TURNS } from "./constants"
 import { checkWinnerFrom, checkEndGame} from "./logic/board"
 import { WinnerModal  } from "./components/WinnerModal"
 
 function App() {
 
-  const [board, setBoard] = useState(Array(9).fill(null))
-  const [turn, setTurn] = useState(TURNS.X)
+  const [board, setBoard] = useState(() => {
+    const boardFromStorage = window.localStorage.getItem('board')
+    if (boardFromStorage) return JSON.parse(boardFromStorage)
+    return Array(9).fill(null)
+  })
+
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem('turn')
+    return turnFromStorage ?? TURNS.X
+  })
   const [winner, setWinner] = useState(null)
 
   
@@ -17,6 +25,9 @@ function App() {
     setBoard(Array(9).fill(null))
     setTurn(TURNS.X)
     setWinner(null)
+
+    window.localStorage.removeItem('board')
+    window.localStorage.removeItem('turn')
   }
 
 
@@ -26,10 +37,14 @@ function App() {
 
     const newBoard = [...board]
     newBoard[index] = turn
-   setBoard(newBoard)
+    setBoard(newBoard)
 
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
+
+    window.localStorage.setItem('board', JSON.stringify(newBoard))
+    window.localStorage.setItem('turn',  newTurn)
+
     const newWinner = checkWinnerFrom(newBoard)
     if (newWinner){
       confetti()
